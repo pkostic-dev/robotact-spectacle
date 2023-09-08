20bc6d99a93bb1ed52cb562086ec3ebc76e4b1da
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2023-09-08 14:22:35 +0200

    Update qt_drague.py

--- theatre/scripts/qt_states/qt_drague.py
+++ theatre/scripts/qt_states/qt_drague.py
@@ -14,6 +14,294 @@
 # 1MG 1MF 1MD
 #    Robot
 
+# Version spectacle : 4 carrées
+# 7 sept.
+
+spectacle = {
+    # QT : Où est le comédien ? Toujours en retard celui-là.
+    # S’il continue je le renvoie. On les connaît ces humains,
+    # s’ils n’ont pas de café, ils ne peuvent pas travailler !
+    'r1_retard' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : 'Où est le comédien ? Toujours en retard celui-là. s’il \
+                continue je le renvoie. On les connaît ces humains, s’ils \
+                n’ont pas de café, ils ne peuvent pas travailler !'
+        },
+        [
+            ('time', 6, 'choice')
+        ]
+    ),
+
+
+    # Comédien : ça va je n’ai que 2 minutes de retard,
+    # je ne marche pas par procédure comme toi QT.
+
+    # QT : De toute façon vous les humains, la ponctualité,
+    # ça vous connaît n'est-ce pas ?
+    # Je vous demanderai de suivre mon protocole de A à Z,
+    # et ça commence par être à l’heure.
+    # Bon, ne perdons pas plus de temps, commençons.
+    'r2_commencons' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : 'De toute façon vous les humains, la ponctualité, ça vous \
+                connaît n\'est-ce pas ? Je vous demanderai de suivre mon \
+                protocole de A à Z, et ça commence par être à l’heure. \
+                Bon, ne perdons pas plus de temps, commençons.'
+        },
+        [
+            ('time', 6, 'choice')
+        ]
+    ),
+
+
+    # Comédien : Ok la rigueur, je me place où ? 
+
+    # Le comédien se place. 
+
+    # QT : Êtes-vous professionnel ? Placez vous.
+    'r3_professionnel' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : 'Êtes-vous professionnel ? Placez vous.'
+        },
+        [
+            ('time', 4, 'choice')
+        ]
+    ),
+
+
+    # 1MD Vous n’êtes pas dans la lumière.
+    '1md' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : 'Vous n’êtes pas dans la lumière.'
+        },
+        [
+            ('time', 10, 'detect_gestures_1')
+        ]
+    ),
+
+
+    # 1MG Vous devez intégrer qu’un humain doit rester dans l’axe visuel des
+    # robots et pas l’inverse.
+    # (un temps)
+    '1mg' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : 'Vous devez intégrer qu’un humain doit rester dans l’axe \
+                visuelle du robot et pas l’inverse.\\pau=500\\Reprenez!'
+        },
+        [
+            ('time', 15, 'detect_gestures_1')
+        ]
+    ),
+
+
+    # 1MF ça fait longtemps que vous ne jouez plus en avant-scène, vous vous
+    # prenez pour qui ?
+    '2mg' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : 'ça fait longtemps que vous ne jouez plus en avant-scène, \
+                vous vous prenez pour qui ?'
+        },
+        [
+            ('time', 15, 'detect_gestures_1')
+        ]
+    ),
+
+
+    # 2MF Écoutez ce que je vous dis et arrêtez avec votre susceptibilité
+    # d’humain.
+    # (un temps)detect_gestures_1
+    # Concentrez-vous
+    '2md' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : 'Écoutez ce que je vous dis et arrêtez avec votre\
+                  susceptibilité d’humain. \\pau=500\\ Concentrez-vous.'
+        },
+        [
+            ('time', 6, 'detect_gestures_2')
+        ]
+    ),
+    # (un temps, le robot s’agace)
+    '2mf_concentrez-vous_2' : (
+        {
+            'e' : 'QT/angry',
+            'g' : 'QT/angry',
+            's' : ''
+        },
+        [
+            ('time', 6, 'detect_gestures_2')
+        ]
+    ),
+
+
+    # [le robot passe à une nouvelle configuration du carré]
+    # Mais que faites-vous en fond de scène ? Approchez-vous, vous savez bien
+    # que vous êtes très limité niveau volume sonore.
+    # (QT regarde le comédien exécuter les actions qui lui demande)
+    '2md_2' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : 'Mais que faites-vous en fond de scène ? Rapprochez-vous du publique, \
+                 vous savez bien que vous êtes très limité niveau volume sonore.'
+        },
+        [
+            ('time', 15, 'detect_gestures_2')
+        ]
+    ),
+
+    # 1MF Recule,, on verra moins que vous jouez mal.
+    # (un temps)
+    '1mg_2' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : 'Reculez.. on verra moins que vous jouez mal.'
+        },
+        [
+            ('time', 15, 'detect_gestures_2')
+        ]
+    ),
+
+
+    # 2MG On n’avait pas dit que vous deviez un peu retravailler votre
+    # carrosserie ? Mettez-vous un peu de l’autre côté.
+    # (QT perd patience)
+    '2mg_2' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : 'On n’avait pas dit que vous deviez un peu retravailler \
+                votre carrosserie ? Mettez-vous un peu de l’autre côté.'
+        },
+        [
+            ('time', 15, 'detect_gestures_2')
+        ]
+    ),
+
+
+    # 2MD Vous ne savez rien faire comme on vous le demande, vous êtes vraiment
+    # binaire comme humain.
+    '1md_2' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : 'Vous ne savez rien faire comme on vous le demande, vous \
+                êtes vraiment binaire comme humain.'
+        },
+        [
+            ('time', 15, 'detect_gestures_2')
+        ]
+    ),
+
+
+    # Le comédien s’agace. 
+
+    # Comédien : Tu sais quoi la ponctualité ? Je vais te refaire ta carrosserie
+    # tu vas comprendre ce qu’il t’arrive. 
+
+    # Il le débranche.
+
+        # Etats hors-scénario
+
+    'begin' : (
+        {},
+        [
+            ('time', 1, 'choice')
+        ]
+    ),
+
+    'choice' : (
+        {
+            'g' : '',
+            's' : '' 
+        },
+        [
+            ('key', '&', 'r1_retard'),
+            ('key', 'é', 'r2_commencons'),
+            ('key', '\"', 'r3_professionnel'),
+
+            # Head
+            ('key', 'z', 'look_up'),
+            ('key', 's', 'look_center'),
+            ('key', 'x', 'look_down'),
+            ('key', 'e', 'look_up_right'),
+            ('key', 'q', 'look_left'),
+            ('key', 'd', 'look_right'),
+            ('key', 'a', 'look_up_left'),
+            ('key', 'w', 'look_down_left'),
+            ('key', 'c', 'look_down_right'),
+
+            ('key', 'r', 'reset_posture'),
+            ('key', '*', 'end'),
+
+            ('key', 'g', 'detect_gestures_1'),
+            ('key', 'G', 'detect_gestures_2'),
+        ]
+    ),
+
+    'detect_gestures_1' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : '' 
+        },
+        [
+            ('gesture', 'cour_1m', '1md'),
+            ('gesture', 'jardin_1m', '1mg'),
+            ('gesture', 'cour_2m', '2md'),
+            ('gesture', 'jardin_2m', '2mg'),
+            ('key', 'c', 'choice'),
+        ]
+    ),
+
+    'detect_gestures_2' : (
+        {
+            'e' : '',
+            'g' : '',
+            's' : '' 
+        },
+        [
+            ('gesture', 'cour_1m', '1md_2'),
+            ('gesture', 'jardin_1m', '1mg_2'),
+            ('gesture', 'cour_2m', '2md_2'),
+            ('gesture', 'jardin_2m', '2mg_2'),
+            ('key', 'c', 'choice'),
+        ]
+    ),
+
+    'reset_posture' : (
+        {
+            'e' : '',
+            'g' : 'QT/neutral',
+        },
+        [
+            ('time', 0.1, 'choice')
+        ]
+    ),
+
+    'end' : (
+        {},
+        [
+            ('time', 0.1, 'end')
+        ]
+    ),
+}
+
+
 # VErsion 4 : 4 carrées 
 # 27 juin
 
@@ -96,7 +384,7 @@ states_v4 = {
             'e' : '',
             'g' : '',
             's' : 'Vous devez intégrer qu’un humain doit rester dans l’axe \
-                visuelle et pas l’inverse.'
+                visuelle du robot et pas l’inverse.'
         },
         [
             ('time', 15, 'detect_gestures_1')
@@ -169,7 +457,7 @@ states_v4 = {
         {
             'e' : '',
             'g' : '',
-            's' : 'Recule.. on verra moins que vous jouez mal.'
+            's' : 'Reculez.. on verra moins que vous jouez mal.'
         },
         [
             ('time', 15, 'detect_gestures_2')
