a3fc6be69d8a1b5299627ead3d4377bd1b169457
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2023-11-25 15:20:29 +0100

    add other behaviors (test)

--- theatre/scripts/spectacle/master.py
+++ theatre/scripts/spectacle/master.py
@@ -1,14 +1,15 @@
 # These states manage switching between different scenes
 
+#! xrandr -o normal
+#! Windows + O
+
 states = {
     'begin': ({}, [('time', 1, 'choice')]),
-    'end': ((), [('time', 0.1, 'end')]) ,
+    'end': ({}, [('time', 0.1, 'end')]) ,
 
-    'choice': (
+    'choice_main': (
         {'g': '', 's': '' },
         [
-            ('key', '&', 'scene_1'),
-            ('key', '"', 'scene_3'),
             ('key', '(', 'scene_5'),
             ('key', "è", 'scene_7'),
             ('key', '_', 'scene_8'),
@@ -17,10 +18,62 @@ states = {
             ('key', 'a', 'scene_11'),
             ('key', 'z', 'scene_12'),
             ('key', 'f', 'scene_fin'),
+
+            ('key', '*', 'end'),
+
+            # TEST
+            ('key', 't', 'talk_en'),
+            ('key', 'T', 'talk_it'),
+            ('key', 'e', 'english'),
+            ('key', 'i', 'italian'),
+            ('key', 's', 'stop_speech'),
         ]
     ),
 
-    'scene_1' : (
-        
-    )
+    # TEST
+
+    'talk_en': (
+        {
+            's': 'Hello, my name is QT Robot!'
+        },
+        [
+            ('time', 0.1, 'choice')
+        ]
+    ),
+
+    'talk_it': (
+        {
+            's': 'Ancora due minuti, ho quasi finito il livello!'
+        },
+        [
+            ('time', 0.1, 'choice')
+        ]
+    ),
+
+    'stop_speech': (
+        {
+            's': '/stop'
+        },
+        [
+            ('time', 0.1, 'choice')
+        ]
+    ),
+
+    'italian': (
+        {
+            's': '/italian'
+        },
+        [
+            ('time', 0.1, 'choice')
+        ]
+    ),
+
+    'english': (
+        {
+            's': '/english'
+        },
+        [
+            ('time', 0.1, 'choice')
+        ]
+    ),
 }
